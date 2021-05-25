1e38bc21a1a8020ba0a66a318815e2bf874d15ba
Puchong <puchong02292@gmail.com>
2021-05-26 04:03:19 +0700

    feat: apply getProducts service to product component

--- components/Product/index.tsx
+++ components/Product/index.tsx
@@ -1,15 +1,36 @@
-import React, { FC } from "react";
-import { mockProducts } from "../../services/getProduct";
+import React, { FC, useEffect, useState } from "react";
+import { getProducts } from "../../services/product";
+import { ProductType } from "../../types/Product";
 
 type ProductPropsType = {
   // props: string;
 };
 
 const Product: FC<ProductPropsType> = () => {
+  const [isLoadingProducts, setIsLoadingProducts] = useState(false);
+  const [productsData, setProductsData] = useState<ProductType[] | []>([]);
+
+  useEffect(() => {
+    async function getInitProductsData() {
+      const initProductsData = await getProducts();
+      if (initProductsData) {
+        setProductsData(initProductsData);
+        setIsLoadingProducts(false);
+      }
+    }
+    setIsLoadingProducts(true);
+    getInitProductsData();
+  }, []);
+
   return (
     <div>
       "Product component"
-      <p>{JSON.stringify(mockProducts)}</p>
+      <br />
+      {isLoadingProducts ? (
+        <p>Loading</p>
+      ) : (
+        <p>{JSON.stringify(productsData)}</p>
+      )}
     </div>
   );
 };
