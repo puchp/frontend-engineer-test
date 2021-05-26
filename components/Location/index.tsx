1c1ab338003e633bfe837e12df9de4a71ed7367c
Puchong <puchong02292@gmail.com>
2021-05-26 14:01:33 +0700

    feat: apply mui in location picker

--- components/Location/index.tsx
+++ components/Location/index.tsx
@@ -1,5 +1,14 @@
 import React, { FC, useEffect, useState } from "react";
-import { ProductType } from "../../types/Product";
+
+import AddIcon from "@material-ui/icons/Add";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+
+import { LocationType } from "../../types/Location";
 import { getLocations } from "../../services/location";
 
 type LocationPropsType = {
@@ -8,7 +17,7 @@ type LocationPropsType = {
 
 const Location: FC<LocationPropsType> = () => {
   const [isLoadingLocations, setIsLoadingLocations] = useState(false);
-  const [locationsData, setLocationsData] = useState<ProductType[] | []>([]);
+  const [locationsData, setLocationsData] = useState<LocationType[] | []>([]);
 
   useEffect(() => {
     async function getInitLocationsData() {
@@ -22,16 +31,73 @@ const Location: FC<LocationPropsType> = () => {
     getInitLocationsData();
   }, []);
 
+  {
+    /* id: "1",
+      createdAt: "2020-06-29T18:20:23.578Z",
+      lat: 13.7370587,
+      long: 100.5603061,
+      name: "Asoke",
+      fee: 1000,
+      max_dist: 1000, 
+     */
+  }
+
   return (
-    <div>
-      "Location component"
-      <br />
+    <Grid container justify="center">
       {isLoadingLocations ? (
-        <p>Loading...</p>
+        <Grid container justify="center" className="m-2">
+          <CircularProgress />
+        </Grid>
       ) : (
-        <p>{JSON.stringify(locationsData)}</p>
+        <Grid container justify="center">
+          <Grid
+            container
+            justify="center"
+            alignContent="center"
+            alignItems="center"
+          >
+            <Grid item xs={3}>
+              <Typography variant="subtitle1">Place</Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="subtitle1">Unit</Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="subtitle1">Cost</Typography>
+            </Grid>
+            <Grid item xs={2} container justify="center">
+              <Button variant="outlined">
+                <AddIcon fontSize="small" />
+              </Button>
+            </Grid>
+          </Grid>
+          {locationsData.map((location: LocationType) => (
+            <Grid
+              key={location.id}
+              container
+              justify="center"
+              alignContent="center"
+              alignItems="center"
+            >
+              <Grid item xs={3}>
+                <Typography variant="subtitle2">{location.name}</Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography variant="subtitle2">{location.fee}</Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography variant="subtitle2">{location.max_dist}</Typography>
+              </Grid>
+              <Grid item xs={2} container justify="center">
+                <IconButton>
+                  <DeleteIcon fontSize="small" />
+                </IconButton>
+              </Grid>
+            </Grid>
+          ))}
+        </Grid>
       )}
-    </div>
+    </Grid>
   );
 };
 
